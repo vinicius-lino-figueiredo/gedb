2c3bec43d50fdd5e3ff4ea334caf58ac3cff961a
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2025-05-30 06:21:59 -0300

    feat: Add basic cursor implementation
    Add cursor implementation with some incomplete methods

---
+++ lib/cursor.go
@@ -0,0 +1,113 @@
+package lib
+
+import (
+	"context"
+	"io"
+
+	"github.com/vinicius-lino-figueiredo/nedb"
+)
+
+// Cursor implements nedb.Cursor.
+type Cursor struct {
+	data       []any
+	index      int64
+	limit      int64
+	projection any
+	sort       any
+	skip       int64
+	storedErr  error
+	query      any
+	mapFn      any
+	ctx        context.Context
+}
+
+// NewCursor returns a new implementation of Cursor
+func NewCursor(ctx context.Context, data []any, options cursorOptions) *Cursor {
+	c := Cursor{
+		ctx:   ctx,
+		query: options.query,
+		mapFn: options.mapFn,
+		data:  data,
+	}
+	return &c
+}
+
+type cursorOptions struct {
+	query      any
+	mapFn      func(any) any
+	limit      int64
+	skip       int64
+	sort       any
+	projection any
+}
+
+// Exec implements nedb.Cursor.
+func (c *Cursor) Exec(ctx context.Context, target any) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+	if c.storedErr != nil {
+		return c.storedErr
+	}
+	if target == nil {
+		return &ErrTargetNil{}
+	}
+	// TODO: Implement cursor deserialization
+	panic("unimplemented")
+}
+
+// ID implements nedb.Cursor.
+func (c *Cursor) ID() [16]byte {
+	// TODO: Implement cursor ID getter
+	panic("unimplemented")
+}
+
+// Limit implements nedb.Cursor.
+func (c *Cursor) Limit(n int64) nedb.Cursor {
+	c.limit = n
+	return c
+}
+
+// Projection implements nedb.Cursor.
+func (c *Cursor) Projection(query any) nedb.Cursor {
+	c.projection = query
+	return c
+}
+
+// Skip implements nedb.Cursor.
+func (c *Cursor) Skip(n int64) nedb.Cursor {
+	c.skip = n
+	return c
+}
+
+// Sort implements nedb.Cursor.
+func (c *Cursor) Sort(query any) nedb.Cursor {
+	c.sort = query
+	return c
+}
+
+// Close implements nedb.Cursor.
+func (c *Cursor) Close() {
+	// TODO: Implement cursor Close function
+	panic("unimplemented")
+}
+
+// Next implements nedb.Cursor.
+func (c *Cursor) Next(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		c.storedErr = ctx.Err()
+		return false
+	default:
+	}
+	c.index++
+	if c.index >= int64(len(c.data)) {
+		c.storedErr = io.EOF
+		return false
+	}
+	return true
+}
+
+var _ nedb.Cursor = (*Cursor)(nil)
---
+++ lib/errors.go
@@ -0,0 +1,7 @@
+package lib
+
+// ErrTargetNil is returned when the passed target, which should be a pointer,
+// is passed as a nil value.
+type ErrTargetNil struct{}
+
+func (e *ErrTargetNil) Error() string { return "target interface is nil" }
