e218c79340ee7720140e3af06fa8387888fb1801
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2025-10-11 10:01:23 -0300

    refactor(Comparer): reduce code complexity

--- internal/adapter/comparer/comparer.go
+++ internal/adapter/comparer/comparer.go
@@ -45,99 +45,158 @@ func (c *Comparer) Comparable(a, b any) bool {
 // Compare implements domain.Comparer.
 func (c *Comparer) Compare(a any, b any) (int, error) {
 
-	// [domain.Getter]
-	if !c.isSet(a) {
-		if !c.isSet(b) {
-			return 0, nil
-		}
-		return -1, nil
-	}
-	if !c.isSet(b) {
-		return 1, nil
+	// [domain.Getter]. Equivalent to js undefined
+	if c, ok, err := c.checkUndefined(a, b); err != nil || ok {
+		return c, err
 	}
 
 	a, b = c.getVal(a), c.getVal(b)
 
-	// nil (undefined/null)
-	if a == nil {
-		if b == nil {
-			return 0, nil
-		}
-		return -1, nil
-	}
-	if b == nil {
-		return 1, nil // no need to test if a == nil
+	// [nil] (null)
+	if c, ok := c.checkNil(a, b); ok {
+		return c, nil
 	}
 
 	// Numbers
+	if c, ok := c.checkNumbers(a, b); ok {
+		return c, nil
+	}
+
+	// Strings
+	if c, ok := c.checkStrings(a, b); ok {
+		return c, nil
+	}
+
+	// Booleans
+	if c, ok := c.checkBooleans(a, b); ok {
+		return c, nil
+	}
+
+	// Dates
+	if c, ok := c.checkTime(a, b); ok {
+		return c, nil
+	}
+
+	// Arrays
+	if c, ok, err := c.checkArrays(a, b); err != nil || ok {
+		return c, err
+	}
+
+	// Objects
+	if c, ok, err := c.checkDocs(a, b); err != nil || ok {
+		return c, err
+	}
+
+	return 0, fmt.Errorf("cannot compare unexpected types %T and %T", a, b)
+}
+
+func (c *Comparer) checkUndefined(a, b any) (int, bool, error) {
+	// [domain.Getter]
+	if !c.isSet(a) {
+		if !c.isSet(b) {
+			return 0, true, nil
+		}
+		return -1, true, nil
+	}
+	if !c.isSet(b) {
+		return 1, true, nil
+	}
+	return 0, false, nil
+}
+
+func (c *Comparer) checkNil(a, b any) (int, bool) {
+	if a == nil {
+		if b == nil {
+			return 0, true
+		}
+		return -1, true
+	}
+	if b == nil {
+		return 1, true // no need to test if a == nil
+	}
+	return 0, false
+}
+
+func (c *Comparer) checkNumbers(a, b any) (int, bool) {
 	if a, ok := c.asNumber(a); ok {
 		// Using big.Float to safely compare float64 and int64 without
 		// precision loss
 		if b, ok := c.asNumber(b); ok {
-			return a.Cmp(b), nil
+			return a.Cmp(b), true
 		}
-		return -1, nil
+		return -1, true
 	}
 	if _, ok := c.asNumber(b); ok {
-		return 1, nil
+		return 1, true
 	}
+	return 0, false
+}
 
-	// Strings
+func (c *Comparer) checkStrings(a, b any) (int, bool) {
 	if a, ok := a.(string); ok {
 		if b, ok := b.(string); ok {
-			return cmp.Compare(a, b), nil
+			return cmp.Compare(a, b), true
 		}
-		return -1, nil
+		return -1, true
 	}
 	if _, ok := b.(string); ok {
-		return 1, nil
+		return 1, true
 	}
+	return 0, false
+}
 
-	// Booleans
+func (c *Comparer) checkBooleans(a, b any) (int, bool) {
 	if a, ok := a.(bool); ok {
 		if b, ok := b.(bool); ok {
-			return c.compareBool(a, b), nil
+			return c.compareBool(a, b), true
 		}
-		return -1, nil
+		return -1, true
 	}
 	if _, ok := b.(bool); ok {
-		return 1, nil
+		return 1, true
 	}
+	return 0, false
+}
 
-	// Dates
+func (c *Comparer) checkTime(a, b any) (int, bool) {
 	if a, ok := a.(time.Time); ok {
 		if b, ok := b.(time.Time); ok {
-			return a.Compare(b), nil
+			return a.Compare(b), true
 		}
-		return -1, nil
+		return -1, true
 	}
 	if _, ok := b.(time.Time); ok {
-		return 1, nil
+		return 1, true
 	}
+	return 0, false
+}
 
-	// Arrays
+func (c *Comparer) checkArrays(a, b any) (int, bool, error) {
 	if a, ok := a.([]any); ok {
 		if b, ok := b.([]any); ok {
-			return c.compareArray(a, b)
+			comp, err := c.compareArray(a, b)
+			return comp, true, err
 		}
-		return -1, nil
+		return -1, true, nil
 	}
 	if _, ok := b.([]any); ok {
-		return 1, nil
+		return 1, true, nil
 	}
+	return 0, false, nil
+}
 
-	// Objects
+func (c *Comparer) checkDocs(a, b any) (int, bool, error) {
 	if a, ok := a.(domain.Document); ok {
 		if b, ok := b.(domain.Document); ok {
-			return c.compareDoc(a, b)
+			comp, err := c.compareDoc(a, b)
+			return comp, true, err
 		}
-		return -1, nil
+		return -1, true, nil
 	}
 	if _, ok := b.(domain.Document); ok {
-		return 1, nil
+		return 1, true, nil
 	}
-
-	return 0, fmt.Errorf("cannot compare unexpected types %T and %T", a, b)
+	return 0, false, nil
 }
 
 func (c *Comparer) compareArray(a, b []any) (int, error) {
